8fc786acc868091d955202d139eac81cc56029c5
Thiago Gentil <thiagosgentil@gmail.com>
2023-04-25 16:51:30 -0300

    Creating CarroDao

---
+++ CarroDao.ts
@@ -0,0 +1,31 @@
+import { DaoInterface } from "./DaoInterface";
+import Carro from "./Carro";
+
+export class CarroDao implements DaoInterface {
+	nomeTabela: string = "tb_Carro";
+
+	inserir(object: Carro): boolean {
+		console.log("logica de insert");
+		return true;
+	}
+
+	atualizar(object: Carro): boolean {
+		console.log("logica update");
+		return true;
+	}
+
+	remover(id: number): boolean {
+		console.log("logica delete");
+		return true;
+	}
+
+	selecionar(id: number) {
+		console.log("logica select");
+		return new Carro("", 0 );
+	}
+
+	selecionarTodos(): [any] {
+		console.log("logica getAll");
+		return [new Carro("", 0)];
+	}
+}
