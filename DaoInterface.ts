86b0f584a5b9626d4380b9d298556f84135520fc
Thiago Gentil <thiagosgentil@gmail.com>
2023-04-25 15:41:03 -0300

    Creating DaoInterface

---
+++ DaoInterface.ts
@@ -0,0 +1,7 @@
+interface DaoInterface {
+    inserir(object: any): boolean;
+    atualizar(object: any): boolean;
+    remover(id: number): boolean;
+    selecionar(id: number): any;
+    selecionarTodos(): [any];
+}
